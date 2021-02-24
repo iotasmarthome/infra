1fde10759153366010474fdccbeec3aea583246a
Duy Dao <daovietduy99@live.com>
2021-02-25 00:41:08 +0700

    rename bucket

--- aws/envs/dev/eks-clusters/terraform.tf
+++ aws/envs/dev/eks-clusters/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate"
+    bucket = "hass-tf-remote-state"
     key    = "dev/eks-clusters/"
     region = "ap-southeast-1"
   }
--- aws/envs/dev/vpc/terraform.tf
+++ aws/envs/dev/vpc/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate"
+    bucket = "hass-tf-remote-state"
     key    = "dev/vpc/"
     region = "ap-southeast-1"
   }
